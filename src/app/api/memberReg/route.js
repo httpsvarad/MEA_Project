1ab1f6267aea2b2ffba3a8484a6838781279d818
Rizvi Ahmed Abbas <92648635+Rizvi-Ahmed-Abbas-Nasir@users.noreply.github.com>
2024-08-05 03:02:24 +0530

    Styled the admin components tem.

--- src/app/api/memberReg/route.js
+++ src/app/api/memberReg/route.js
@@ -9,8 +9,8 @@ export async function POST(req) {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const query = "INSERT INTO memberReg (fullName, email, contactNumber, designation, password, Role) values(?,?,?,?,?,?)";
-    const values = [fullName, email, contactNumber, designation, hashedPassword, "admin" ];
+    const query = "INSERT INTO memberReg (fullName, email, contactNumber, designation, password) values(?,?,?,?,?)";
+    const values = [fullName, email, contactNumber, designation, hashedPassword ];
 
     const result = await executeQuery({ query, values }); 
 
