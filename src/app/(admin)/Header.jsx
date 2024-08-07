f4310f3b20d2e7b7c5e6e25e899a3a24c6cd66cd
Pranav Molawade <pranavmolawade123@gmail.com>
2024-08-07 21:37:09 +0530

    Go back to Home

--- src/app/(admin)/Header.jsx
+++ src/app/(admin)/Header.jsx
@@ -5,9 +5,9 @@ export default function Headers() {
     <div className="w-full  h-full  flex flex-col">
       <ul className="flex flex-col items-center py-4 space-y-2">
         <li className="w-full">
-          <Link href="/Admin" className="group">
+          <Link href="/" className="group">
             <p className="text-white text-[0.9rem] xl:text-[1.2rem] h-[7vh] flex items-center justify-center border-y-[2px] border-transparent group-hover:border-red-500 group-hover:bg-red-500 w-full transition-all duration-300 ease-in-out">
-              Home
+             Go back to Home
             </p>
           </Link>
         </li>
